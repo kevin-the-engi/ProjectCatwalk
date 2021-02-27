65ec8a1a6d6d44966a834e9da9d18394fb839ff8
Kevin Ng <kevin.the.engi@gmail.com>
2021-02-27 14:46:59 -0800

    set up req routes for questions

--- server/routes/questions.route.js
+++ server/routes/questions.route.js
@@ -1,5 +1,41 @@
-var express = require('express')
-var app = express()
-var route = express.Router()
+var express = require('express');
+const TOKEN = require('../../Questions/component/config.js');
+const axios = require('axios');
+
+var app = express();
+var route = express.Router();
+const API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
+
+route.get('/qa/questions', (req, res) => {
+  res.send('Calm down, I am trying to GET your questions');
+});
+
+route.get('/qa/questions/:question_id/answers', (req, res) => {
+  res.send('What kind of answer were you expecting to GET?');
+});
+
+route.post('/qa/questions/', (req, res) => {
+  res.send('I see you trying to POST a question.');
+});
+
+route.post('/qa/questions/:question_id/answers', (req, res) => {
+  res.send('I see you trying to POST an answer.');
+})
+
+route.put('/qa/questions/:question_id/helpful', (req, res) => {
+  res.send('Let us PUT a pin on that helpful question');
+});
+
+route.put('/qa/questions/:question_id/report', (req, res) => {
+  res.send('PUT your hands up! You are reported for a bad question!');
+});
+
+route.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  res.send('Your answer was actually helpful so let\'s PUT that up.');
+})
+
+route.put('/qa/answers/:answer_id/report', (req, res) => {
+  res.send('I don\'t have to PUT up with your lame answer!');
+})
 
 module.exports.route = route;
