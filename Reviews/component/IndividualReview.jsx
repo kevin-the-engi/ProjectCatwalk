27cc47697391b2a4181084fc18f66e005f1609a1
Britta Kotov <brittakotov@gmail.com>
2021-02-27 21:13:43 -0800

    formatted individual reviews with css, updated package.json jest config to allow for css modules styling

--- Reviews/component/IndividualReview.jsx
+++ Reviews/component/IndividualReview.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styles from './IndividualReview.module.css';
+//import Date from './Date.jsx'
 
 const IndividualReview = (props) => {
   return (
-    <div>
-      <div>
-        Summary: {props.review.summary}
+    <div className={styles.individualReview}>
+      <div className={styles.header}>
+        <div>
+        Star Rating
+        </div>
+        <div className={styles.headerRight}>
+          <div>
+            {props.review.reviewer_name}
+          </div>
+          <div>
+            {props.review.date}
+          </div>
+        </div>
+      </div>
+      <div className={styles.summary}>
+        {props.review.summary}
       </div>
       <div>
-        Body: {props.review.body}
+        {props.review.body}
+      </div>
+      <div>
+        Helpful and report
       </div>
     </div>
   )
