a5f0bed90dae31afc131602da059ccbbe2c900cd
Kevin Ng <kevin.the.engi@gmail.com>
2021-02-27 17:34:39 -0800

    added controllers for question routes

---
+++ server/controllers/questions.controller.js
@@ -0,0 +1,68 @@
+const TOKEN = require('../../Questions/component/config.js');
+const axios = require('axios');
+const API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
+
+const options = {
+  headers: {
+    'Access-Control-Allow-Headers' : "*",
+    'User-Agent': 'request',
+    'Authorization': `Bearer ${TOKEN}`
+  }
+};
+
+console.log(options);
+
+const readQuestions = (req, res) => {
+  let id = req.query.product_id;
+  let query = API + req.path + '?product_id=' + id;
+  // console.log(query);
+
+  axios.get(query, options)
+    .then(questions => {
+      res.status(200).send(questions);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    })
+  // res.send('Calm down, I am trying to GET your questions');
+};
+
+const readAnswers = (req, res) => {
+  res.send('What kind of answer were you expecting to GET?');
+};
+
+const createQuestions = (req, res) => {
+  res.send('I see you trying to POST a question.');
+};
+
+const createAnswers = (req, res) => {
+  res.send('I see you trying to POST an answer.');
+};
+
+const updateHelpfulQ = (req, res) => {
+  res.send('Let us PUT a pin on that helpful question');
+};
+
+const updateReportQ = (req, res) => {
+  res.send('PUT your hands up! You are reported for a bad question!');
+};
+
+const updateHelpfulA = (req, res) => {
+  res.send('Your answer was actually helpful so let\'s PUT that up.');
+};
+
+const updateReportA = (req, res) => {
+  res.send('I don\'t have to PUT up with your lame answer!');
+};
+
+module.exports = {
+  readQuestions,
+  readAnswers,
+  createQuestions,
+  createAnswers,
+  updateHelpfulQ,
+  updateReportQ,
+  updateHelpfulA,
+  updateReportA
+};
--- server/routes/questions.route.js
+++ server/routes/questions.route.js
@@ -1,41 +1,23 @@
-var express = require('express');
-const TOKEN = require('../../Questions/component/config.js');
-const axios = require('axios');
+const express = require('express');
+const api = require('../controllers/questions.controller.js');
 
-var app = express();
-var route = express.Router();
-const API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
+const app = express();
+const route = express.Router();
 
-route.get('/qa/questions', (req, res) => {
-  res.send('Calm down, I am trying to GET your questions');
-});
+route.get('/qa/questions', api.readQuestions);
 
-route.get('/qa/questions/:question_id/answers', (req, res) => {
-  res.send('What kind of answer were you expecting to GET?');
-});
+route.get('/qa/questions/:question_id/answers', api.readAnswers);
 
-route.post('/qa/questions/', (req, res) => {
-  res.send('I see you trying to POST a question.');
-});
+route.post('/qa/questions/', api.createQuestions);
 
-route.post('/qa/questions/:question_id/answers', (req, res) => {
-  res.send('I see you trying to POST an answer.');
-})
+route.post('/qa/questions/:question_id/answers', api.createAnswers);
 
-route.put('/qa/questions/:question_id/helpful', (req, res) => {
-  res.send('Let us PUT a pin on that helpful question');
-});
+route.put('/qa/questions/:question_id/helpful', api.updateHelpfulQ);
 
-route.put('/qa/questions/:question_id/report', (req, res) => {
-  res.send('PUT your hands up! You are reported for a bad question!');
-});
+route.put('/qa/questions/:question_id/report', api.updateReportQ);
 
-route.put('/qa/answers/:answer_id/helpful', (req, res) => {
-  res.send('Your answer was actually helpful so let\'s PUT that up.');
-})
+route.put('/qa/answers/:answer_id/helpful', api.updateHelpfulA);
 
-route.put('/qa/answers/:answer_id/report', (req, res) => {
-  res.send('I don\'t have to PUT up with your lame answer!');
-})
+route.put('/qa/answers/:answer_id/report', api.updateReportA);
 
 module.exports.route = route;
