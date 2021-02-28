54e4dfeb2314a78318e6ec13cdaa9e7486ddca35
Kevin Ng <kevin.the.engi@gmail.com>
2021-02-27 21:11:11 -0800

    implemented answer date

--- Questions/component/QList/QListA.jsx
+++ Questions/component/QList/QListA.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 const QListA = (props) => {
   // console.log(props);
+  let date = props.answer.date.slice(0, 10).split('-');
+  let year = Number(date[0]);
+  let month = Number(date[1]);
+  let day = Number(date[2]);
+  date = new Date(year, month, day)
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: '2-digit'}).format(date);
+
   return(
     <div>
       <p>A: {props.answer.body}</p>
-      <p>by {props.answer.answerer_name}</p>
+      <p>by {props.answer.answerer_name}, {formattedDate}</p>
     </div>
   );
 };
