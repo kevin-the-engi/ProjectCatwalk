94e1c19d1299923d8f8cd507272e2bc0d9744f19
Kevin Ng <kevin.the.engi@gmail.com>
2021-02-28 19:54:44 -0800

    set up server side

--- server/controllers/questions.controller.js
+++ server/controllers/questions.controller.js
@@ -4,69 +4,111 @@ const API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
 
 const options = {
   headers: {
-    'Authorization': TOKEN
+    Authorization: TOKEN
   }
 };
 
-console.log(options);
 
 const readQuestions = (req, res) => {
   let id = req.query.product_id;
-  let query = API + req.path + '?product_id=' + id;
-  console.log(query);
+  let page = req.query.page || 1;
+  let count = req.query.count || 5;
+  let query = API + req.path + `?product_id=${id}&page=${page}&count=${count}`;
 
-  axios({
-    method: 'get',
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions/${id}`,
-    headers: {
-      Authorization: TOKEN
-    }
-  })
-  .then(questions => {
-    res.status(200).send(questions);
+  axios.get(query, options)
+  .then(answers => {
+    res.status(200).send(answers.data);
   })
   .catch(err => {
     console.log(err);
-    res.send(500);
+    res.sendStatus(500);
   })
-
-  // axios.get(query, options)
-  //   .then(questions => {
-  //     res.status(200).send(questions);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //     res.sendStatus(500);
-  //   })
-  // res.send('Calm down, I am trying to GET your questions');
 };
 
 const readAnswers = (req, res) => {
-  res.send('What kind of answer were you expecting to GET?');
+  let id = req.params.question_id;
+
+  axios.get(API + req.path, options)
+  .then(answers => {
+    res.status(200).send(answers.data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  })
 };
 
 const createQuestions = (req, res) => {
-  res.send('I see you trying to POST a question.');
+  axios.post(API + req.path, req.body, options)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    })
+
+  // res.send('I see you trying to POST a question.');
 };
 
 const createAnswers = (req, res) => {
-  res.send('I see you trying to POST an answer.');
+  axios.post(API + req.path, req.body, options)
+  .then(() => {
+    res.sendStatus(201);
+  })
+  .catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  })
+  // res.send('I see you trying to POST an answer.');
 };
 
 const updateHelpfulQ = (req, res) => {
-  res.send('Let us PUT a pin on that helpful question');
+  axios.put(API + req.path, {} ,options)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  })
+  // res.send('Let us PUT a pin on that helpful question');
 };
 
 const updateReportQ = (req, res) => {
-  res.send('PUT your hands up! You are reported for a bad question!');
+  axios.put(API + req.path, {} ,options)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  })
+  // res.send('PUT your hands up! You are reported for a bad question!');
 };
 
 const updateHelpfulA = (req, res) => {
-  res.send('Your answer was actually helpful so let\'s PUT that up.');
+  axios.put(API + req.path, {} ,options)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  })
+  // res.send('Your answer was actually helpful so let\'s PUT that up.');
 };
 
 const updateReportA = (req, res) => {
-  res.send('I don\'t have to PUT up with your lame answer!');
+  axios.put(API + req.path, {} ,options)
+  .then(() => {
+    res.sendStatus(204);
+  })
+  .catch(err => {
+    console.log(err);
+    res.sendStatus(500);
+  })
+  // res.send('I don\'t have to PUT up with your lame answer!');
 };
 
 module.exports = {
