d770f4e4580bb78ea3cb86dae9877f10656c3b61
Ika Ogeil <nahlaogeil@gmail.com>
2021-03-02 00:01:43 -0500

    connected client to server to render gallery image and thumbnails

--- Product/component/Product.jsx
+++ Product/component/Product.jsx
@@ -3,16 +3,50 @@ import ReactDOM from 'react-dom'
 import styles from './product.module.css'
 import Gallery from './PGallery.jsx'
 import Info from './PInfo.jsx'
+import axios from 'axios';
 
 class Product extends React.Component {
   constructor () {
     super()
+    this.state = {
+    id: 14807,
+    image: '',
+    thumbnails: []
+  }
+}
+
+  componentDidMount() {
+    // hardcoded product_id temporarily for MVP
+    this.setState({id: 14807})
+    axios.get('/product', {
+      params: {
+          id: this.state.id
+      }
+    })
+    .then((response) => {
+      var results = response.data.results;
+      var image;
+      var thumbnails = [];
+      console.log(results)
+      for(var i = 0; i < results.length; i++) {
+        if (results[i]['default?'] === true) {
+          var image = results[i].photos[0].thumbnail_url;
+          console.log('image object: ', image)
+        }
+        thumbnails.push(results[i].photos[0].thumbnail_url);
+
+      }
+      this.setState({
+        image: image,
+        thumbnails: thumbnails
+      });
+    })
   }
 
   render () {
     return(
-      <div className={styles.body}>
-        <Gallery />
+      <div className={styles.context}>
+        <Gallery image={this.state.image} thumbnails={this.state.thumbnails}/>
         <Info />
       </div>
     )
